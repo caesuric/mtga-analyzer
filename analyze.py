41017be23e457489172ca82d852348702dccae8a
caesuric <illusorycaesura@gmail.com>
2019-02-10 13:03:31 -0500

    fixed some nomenclature issues - I had the terminology for 'ranks' and 'tiers' backwards

--- analyze.py
+++ analyze.py
@@ -1,21 +1,21 @@
 import argparse
 import random
 
-def main(winrate, tier, rank, subrank, num_games):
-    show_tier(winrate, tier, rank, subrank)
+def main(winrate, rank, tier, subtier, num_games):
+    show_rank(winrate, tier, rank, subtier)
     random.seed()
     total = 0.0
     for i in range(num_games):
-        sim = Sim(winrate, tier, rank, subrank)
+        sim = Sim(winrate, rank, tier, subtier)
         total += sim.run()
     total /= float(num_games)
-    print('Need to play {0} games on average to rank up to {1}'.format(total, get_next_tier(tier)))
+    print('Need to play {0} games on average to rank up to {1}'.format(total, get_next_rank(rank)))
 
-def show_tier(winrate, tier, rank, subrank):
-    print('{0} tier {1}, with {2} bars'.format(tier, rank, subrank))
+def show_rank(winrate, rank, tier, subtier):
+    print('{0} tier {1}, with {2} bars'.format(rank, tier, subtier))
     print('Assuming {0}% winrate'.format(winrate*100.0))
 
-def get_next_tier(tier):
+def get_next_rank(rank):
     results = {
         'bronze': 'silver',
         'silver': 'gold',
@@ -23,28 +23,28 @@ def get_next_tier(tier):
         'platinum': 'diamond',
         'diamond': 'mythic'
     }
-    return results[tier]
+    return results[rank]
 
 class Sim():
-    def __init__(self, winrate, tier, rank, subrank):
+    def __init__(self, winrate, rank, tier, subtier):
         self.winrate = winrate
-        self.rank = self.determine_rank(tier, rank, subrank)
-        self.tier = tier
+        self.subtier = self.determine_tier(rank, tier, subtier)
+        self.rank = rank
     
-    def determine_rank(self, tier, rank, subrank):
-        subranks_per_tier = {
+    def determine_tier(self, rank, tier, subtier):
+        subtiers_per_tier = {
             'bronze': 4,
             'silver': 5,
             'gold': 6,
             'platinum': 7,
             'diamond': 7
         }
-        total_subranks = 4 * subranks_per_tier[tier]
-        return total_subranks - 1 - (rank*subranks_per_tier[tier]) + subrank
+        total_subtiers = 4 * subtiers_per_tier[rank]
+        return total_subtiers - 1 - (tier*subtiers_per_tier[rank]) + subtier
     
     def run(self):
         games = 0
-        while self.rank<23:
+        while self.subtier<23:
             self.run_game()
             games += 1
         return games
@@ -55,25 +55,25 @@ class Sim():
             self.process_win()
         else:
             self.process_loss()
-        if self.rank<0:
-            self.rank = 0
+        if self.subtier<0:
+            self.subtier = 0
     
     def process_win(self):
-        if self.tier in ['bronze', 'silver']:
-            self.rank += 2
+        if self.rank in ['bronze', 'silver']:
+            self.subtier += 2
         else:
-            self.rank += 1
+            self.subtier += 1
     
     def process_loss(self):
-        if self.tier!='bronze':
-            self.rank -= 1
+        if self.rank!='bronze':
+            self.subtier -= 1
 
 if __name__=='__main__':
-    parser = argparse.ArgumentParser(description='Analyze games to rank up to the next tier in MTGA based on winrate.')
-    parser.add_argument('--winrate', type=float, help='Your winrate at the current tier.', default=0.5)
-    parser.add_argument('--tier', type=str, help='Current tier', default='gold', choices=['bronze', 'silver', 'gold', 'platinum', 'diamond'])
-    parser.add_argument('--rank', type=int, help='Your rank at the current tier.', default=4)
-    parser.add_argument('--subrank', type=int, help='Your "blocks" on the meter at the current rank.', default=0)
+    parser = argparse.ArgumentParser(description='Analyze games to rank up to the next rank in MTGA based on winrate.')
+    parser.add_argument('--winrate', type=float, help='Your winrate at the current rank.', default=0.5)
+    parser.add_argument('--rank', type=str, help='Current rank', default='gold', choices=['bronze', 'silver', 'gold', 'platinum', 'diamond'])
+    parser.add_argument('--tier', type=int, help='Your tier at the current rank.', default=4)
+    parser.add_argument('--subtier', type=int, help='Your "blocks" on the meter at the current tier.', default=0)
     parser.add_argument('--runs', type=int, help='Sim runs to execute. Increases execution time and accuracy.', default=10000)
     args = parser.parse_args()
-    main(args.winrate, args.tier, args.rank, args.subrank, args.runs)
+    main(args.winrate, args.rank, args.tier, args.subtier, args.runs)
